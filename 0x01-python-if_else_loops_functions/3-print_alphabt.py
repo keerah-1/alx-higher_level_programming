fb43d78c3405a80efae5a2d2a8968ca83f5f5414
keerah-1 <danaezeuko@gmail.com>
2022-06-01 14:33:20 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
     if (f"{ch:c}") != "q" and (f"{ch:c}") != "e":
-        print("{:c}".format(ch), end="")
-        
+        print("{:c}".format(ch), end="") 
